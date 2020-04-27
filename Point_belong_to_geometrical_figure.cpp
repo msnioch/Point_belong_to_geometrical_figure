6fd577e00f1535f98d3633f1ca9d58d8561973c4
Mateusz Snioch <mateusz.snioch@gmail.com>
2020-04-27 19:28:58 +0200

    Update Point_belong_to_geometrical_figure.cpp
    Added comments.

--- Point_belong_to_geometrical_figure.cpp
+++ Point_belong_to_geometrical_figure.cpp
@@ -12,11 +12,11 @@ double Ax = 0, Ay = 0, Bx = 4, By = 0, Cx = 2, Cy = 4, Mx = 2, My = 2;
 int main()
 {
     
-    auto Pythagoras = [](auto x1, auto y1, auto x2, auto y2) {return hypot(x1 - x2, y1 - y2); };
+    auto Pythagoras = [&](auto x1, auto y1, auto x2, auto y2) {return hypot(x1 - x2, y1 - y2); };
 
-    auto Field = [](auto x1, auto y1, auto x2, auto y2, auto x3, auto y3) {return 0.5 * abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)); };
+    auto Field = [&](auto x1, auto y1, auto x2, auto y2, auto x3, auto y3) {return 0.5 * abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)); };
     
-    cout << "##### Point belong to figure #####\n##### Main Menu #####" << endl;
+    cout << "##### Point belong to figure #####\n########## Main Menu ##########" << endl;
 
     cout << "Which geometrical figure you want to check the belonging of the point M ?" << endl;
 
@@ -26,29 +26,36 @@ int main()
 
     char switch_on = _getch();
 
+    cout << endl;
+
     switch (switch_on)
     {
+    default:
+
+        cout << "The function number is false!\nPlease enter the correctly function number." << endl;
+
+        break;
 
     case '1':
+
+        cout << "Please enter vertices coordinates of the triangle and M point coordinates\nin order: Ax Ay Bx By Cx Cy Mx My: ";
+
+        cin >> Ax >> Ay >> Bx >> By >> Cx >> Cy >> Mx >> My;
 
         auto AB_Length = Pythagoras(Ax, Ay, Bx, By);
 
         auto BC_Length = Pythagoras(Bx, By, Cx, Cy);
 
-        auto AC_Length = Pythagoras(Ax, Ay, Cx, Cy);
+        auto CA_Length = Pythagoras(Cx, Cy, Ax, Ay);
 
         auto Field_ABC = Field(Ax, Ay, Bx, By, Cx, Cy);
 
         auto FieldSumary = Field(Ax, Ay, Bx, By, Mx, My) + Field(Bx, By, Cx, Cy, Mx, My) + Field(Ax, Ay, Cx, Cy, Mx, My);
 
-        cout << "AB side length = " << AB_Length << "\nBC side length = " << BC_Length << "\nAC side length = " << AC_Length << "\nABC Triangle area = " << Field_ABC << endl;
+        cout << "AB side length = " << AB_Length << "\nBC side length = " << BC_Length << "\nCA side length = " << CA_Length << "\nABC Triangle area = " << Field_ABC << endl;
 
-        (Field_ABC == FieldSumary) ? cout << "The point belong to triangle ABC." << endl : cout << "The point don't belong to triangle ABC." << endl;
+        (Field_ABC == FieldSumary) ? cout << "Point M(" << Mx << ", " << My << ") belong to triangle ABC." << endl : cout << "Point M(" << Mx << ", " << My << ") don't belong to triangle ABC." << endl;
         
-        break;
-
-    default:
-
         break;
     }
 
